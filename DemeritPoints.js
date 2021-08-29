574245b92e19250e1c2032dffc295e466660cb67
MaSsoumeh <massoumehzarei@gmail.com>
2021-08-29 08:12:42 +0430

    add JS code to the repository

---
+++ DemeritPoints.js
@@ -0,0 +1,16 @@
+﻿'use strict'
+function checkSpeed(speed) {
+
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint) {
+        return "ok";
+    }
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points < 12) {
+        return points;
+    }
+    return "License suspended";
+
+}
